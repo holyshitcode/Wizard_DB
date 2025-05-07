ec7f02b8187aecf607447c2b72c36559f2b4eb4c
junyeong <557987@naver.com>
2025-05-07 20:41:09 +0900

    insert & delete key from chain added

--- chain.c
+++ chain.c
@@ -11,3 +11,20 @@ struct Chain *create_chain(char *chain_name) {
     return chain;
 }
 
+int insert_key_to_chain(struct Chain *target_chain, struct Key *key) {
+    struct Node *target_chain_list = target_chain->node_list;
+
+    if(insert_key_into_node(target_chain_list, key) == 0) {
+        return 0;
+    }
+    return -1;
+}
+
+int delete_key_from_chain(struct Chain *target_chain, struct Key *key) {
+    struct Node *target_chain_list = target_chain->node_list;
+
+    if(delete_node_having_key(target_chain_list, key) == 0) {
+        return 0;
+    }
+    return -1;
+}
--- chain.h
+++ chain.h
@@ -1,6 +1,7 @@
 #ifndef CHAIN_H
 #define CHAIN_H
 
+#include "data.h"
 #include "linked_list.h"
 #include "key.h"
 
@@ -11,5 +12,7 @@ struct Chain{
 };
 
 struct Chain *create_chain(char *chain_name);
+int insert_key_to_chain(struct Chain *target_chain, struct Key *key);
+int delete_key_from_chain(struct Chain *target_chain, struct Key *key);
 
 #endif
